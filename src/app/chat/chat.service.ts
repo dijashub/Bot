12d3d43d8dd1cf40f55146b59d1d205b860f7c47
sajid <sajans.sajids@gmail.com>
2020-06-03 22:10:13 +0100

    hi

---
+++ src/app/chat/chat.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment'
+
+import { ApiAiClient } from "api-ai-javascript/es6/ApiAiClient"
+import { Observable} from 'rxjs/Observable'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+
+
+export class Message {
+  constructor(public content : string, public sentBy : string) {
+
+  }
+}
+@Injectable()
+export class ChatService {
+
+  readonly token = environment.dialogflow.angularBot;
+  readonly client = new ApiAiClient({accessToken: this.token});
+
+  conversation = new BehaviorSubject<Message[]>([]);
+
+  constructor() { }
+
+
+
+  update(msg : Message){
+    this.conversation.next([msg]);
+  }
+
+  converse(msg: string) {
+    const userMessage = new Message(msg, 'Human')
+    this.update(userMessage)
+    return this.client.textRequest(msg).then (res => {
+      const speech = res.result.fulfillment.speech;
+      const botMessage = new Message(speech, 'BOT')
+      this.update(botMessage)
+    })
+
+  }
+  talk() {
+
+    this.client.textRequest('Who are you!')
+    .then(res => console.log(res))
+  }
+
+}
